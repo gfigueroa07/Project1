6757fa153d935baa77355e79c41a0400fd38765b
gfigueroa07 <132857368+gfigueroa07@users.noreply.github.com>
2025-04-04 10:27:42 -0400

    I created a function that validates user input into numbers, or prompts user to exeit the program at any time.

--- calculator.py
+++ calculator.py
@@ -1,47 +1,22 @@
-# # Multiply
-# def multiply(x, y):
-#     answer = x * y
-#     return answer
-# # Divide
-# def division(x, y):
-#     answer = x / y
-#     return answer
-# # Substract
-# def substract(x,y):
-#     answer = x - y
-#     return answer
-# # Add
-# def add(x, y):
-#     answer = x + y
-#     return answer
 
-# # User input to use calculator
 
-# user_answer = False
-# while not user_answer:
-#     user_answer = input("What would you like to do?\n1. Multiply\n2. Divide\n3. Add\n4. Substract\nEnter your answer (1,2,3,4) or 'q' to Quit:\n")
-#     if user_answer == "q":
-#         print("You have exited the program.")
-#         user_answer = True
-#     elif user_answer == "1":
-#         num_1 = int(input("Choose your first number:\n"))
-#         num_2 = int(input("Choose your second number:\n"))
-#         print(f"Your total is: {multiply(num_1, num_2)}")
-#     elif user_answer == "2":
-#         num_1 = int(input("Choose your first number:\n"))
-#         num_2 = int(input("Choose your second number:\n"))
-#         print(f"Your total is: {division(num_1, num_2)}")
-#     elif user_answer == "3":
-#         num_1 = int(input("Choose your first number:\n"))
-#         num_2 = int(input("Choose your second number:\n"))
-#         print(f"Your total is: {add(num_1, num_2)}")
-#     elif user_answer == "4":
-#         num_1 = int(input("Choose your first number:\n"))
-#         num_2 = int(input("Choose your second number:\n"))
-#         print(f"Your total is: {substract(num_1, num_2)}")
-#     else:
-#         user_answer = False
 
+
+def input_to_int_():
+    while True:
+        try:
+            numbers = ("0", "1", "2", "3", "4", "5", "6", "7", "8", "9")
+            num1 = int(input("Choose your first number: "))
+            num2 = int(input("Choose your second number: "))
+            user_choice = input("Enter operator (+, -, *, /) or 'q' to exit: ")
+        
+        
+        except ValueError as e:
+            return None
+        except ZeroDivisionError as e:
+            print("Can't divide by zero. Choose a valid number")
+        except Exception as e:
+            print('Error:', e)
 
 
 def calculate():
