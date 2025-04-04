b74fa4043c80698d9f6243511a130bb785f9ec92
gfigueroa07 <132857368+gfigueroa07@users.noreply.github.com>
2025-04-04 10:29:23 -0400

    validate user input or prompt to exit pogram

--- calculator.py
+++ calculator.py
@@ -1,23 +1,36 @@
 
 
-
-
+# Validate user input or prompt user to exit program
 def input_to_int_():
     while True:
-        try:
-            numbers = ("0", "1", "2", "3", "4", "5", "6", "7", "8", "9")
-            num1 = int(input("Choose your first number: "))
-            num2 = int(input("Choose your second number: "))
-            user_choice = input("Enter operator (+, -, *, /) or 'q' to exit: ")
-        
-        
-        except ValueError as e:
+    # Take 1st valid number from user
+        user_choice1 = input("Choose your first number or enter 'q' to exit: ")
+    # Exit program if user enters "q"
+        if user_choice1.lower() == "q":
+            print("Exiting the program")
             return None
-        except ZeroDivisionError as e:
-            print("Can't divide by zero. Choose a valid number")
-        except Exception as e:
-            print('Error:', e)
-
+        try:
+    # Convert input into valid number
+            num1 = int(user_choice1)
+            break
+        except ValueError:
+            print("Please enter a valid number or enter 'q' to exit: ")
+    while True:
+    # Take 2nd valid number from user
+        user_choice2 = input("Choose your first number or enter 'q' to exit: ")
+    # Exit program if user enters "q"
+        if user_choice2.lower() == "q":
+            print("Exiting program")
+            return None
+        try:
+    # Convert input into valid number
+            num2 = int(user_choice2)
+            break
+        except ValueError:
+            print("Please enter a valid number")
+    return num1, num2
+calc = input_to_int_()
+print(calc)
 
 def calculate():
     while True:
